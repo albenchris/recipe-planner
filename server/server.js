482031ffad96113d9978c68054b0717c98c18443
albenchris <albenchris00@gmail.com>
2023-07-03 11:05:43 -0500

    hit the edamom route, unauthorized

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
 // Server
+const { get } = require('axios');
+const _ = require('lodash');
+// const { red, yellow, blue, cyan } = import('chalk');
+const log = console.log;
+
+const edamam = 'https://api.edamam.com/api'
+
+get(`${edamam}/recipes/v2`)
+  .then(res => {
+    // log(cyan(res));
+    log(res);
+  })
+  .catch(err => console.error(err));
