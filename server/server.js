21b7968ea72ab2f5b645a117f92c87e4383385f6
albenchris <albenchris00@gmail.com>
2023-07-04 18:30:51 -0500

    cleaning up the request a bit

--- server/server.js
+++ server/server.js
@@ -9,8 +9,12 @@ const edamam = 'https://api.edamam.com/api'
 
 const EDAMAM_APP_ID = process.env.EDAMAM_APP_ID;
 const EDAMAM_APP_KEY = process.env.EDAMAM_APP_KEY;
+const requiredText = `type=public&app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_APP_KEY}`
+const query = 'eggs'
+const mealType = 'breakfast'
+const random = true
 
-get(`${edamam}/recipes/v2?q=eggs&mealType=breakfast&type=public&random=true&app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_APP_KEY}`, {
+get(`${edamam}/recipes/v2?q=${query}&mealType=${mealType}&random=${random}&${requiredText}`, {
   headers: {
     'Accept': 'application/json',
     'Accept-Language': 'en',
