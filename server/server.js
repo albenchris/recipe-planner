024aafc2cc9294884e845d9d7c2674a449bb10ef
albenchris <albenchris00@gmail.com>
2023-07-04 18:25:48 -0500

    successful response from edamam api

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // Server
+require('dotenv').config();
 const { get } = require('axios');
 const _ = require('lodash');
 // const { red, yellow, blue, cyan } = import('chalk');
@@ -6,9 +7,29 @@ const log = console.log;
 
 const edamam = 'https://api.edamam.com/api'
 
-get(`${edamam}/recipes/v2`)
+const EDAMAM_APP_ID = process.env.EDAMAM_APP_ID;
+const EDAMAM_APP_KEY = process.env.EDAMAM_APP_KEY;
+
+get(`${edamam}/recipes/v2?q=eggs&mealType=breakfast&type=public&random=true&app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_APP_KEY}`, {
+  headers: {
+    'Accept': 'application/json',
+    'Accept-Language': 'en',
+  }
+})
   .then(res => {
-    // log(cyan(res));
-    log(res);
+    // log(res.data);
+    const hits = res.data.hits;
+    log(hits[0].recipe.label);
+    log(hits[0].recipe.uri);
+    log(hits[0].recipe.source);
+    log(hits[0].recipe.cautions);
+    log(hits[0].recipe.calories);
+    log(hits[0].recipe.ingredientLines);
+    
+
+    // hits.map(hit => {
+    //   log(hit.recipe);
+    //   log(hit._links.self.href);
+    // });
   })
   .catch(err => console.error(err));
